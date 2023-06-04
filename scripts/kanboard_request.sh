5dd8d6db6d2fabade172ced688a9f0f971ba1366
Alexey <moogur-mail@yandex.ru>
2023-06-04 19:52:29 +0300

    [SRV-184] feature(workflow): add github actions for kanboard tasks

--- scripts/kanboard_request.sh
+++ scripts/kanboard_request.sh
@@ -17,10 +17,13 @@ EOF
 
 function request() {
   local method=$1
-  local params=$2
 
-  if [[ $params =~ " " ]]; then
-    params=`echo {$params} | tr " " ","`
+  if [[ $2 =~ " " ]]; then
+    echo $2
+    params=`echo {$2} | tr " " ","`
+    echo $params
+  else
+    params=$2
   fi
 
   result=$(curl -u "$auth_data" -d "$(generate_post_data $method $params)" $url/jsonrpc.php)
