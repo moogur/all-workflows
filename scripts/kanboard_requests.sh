0b43a6780dda5328f58ed9fe355772eb6d51bb82
Alexey <moogur-mail@yandex.ru>
2023-06-10 13:29:19 +0300

    [SRV-184] feature(workflow): update kanboard workfow

--- scripts/kanboard_requests.sh
+++ scripts/kanboard_requests.sh
@@ -120,9 +120,19 @@ function save_separator_in_file() {
   fi
 }
 
+function save_raw_message_in_file() {
+  local raw_message=$1
+  local result=$2
+
+  if [[ $result != 'true' ]]; then
+    echo "Raw error - '$raw_message'" >> $private_file_path
+  fi
+}
+
 function save_message_in_file() {
   local column_name=$1
   local task_id=$2
+  local raw_message=$3
   local result=$3
 
   if [[ $task_id == "-1" ]]; then
@@ -145,22 +155,26 @@ function save_message_in_file() {
       ;;
   esac
 
+  save_raw_message_in_file $raw_message $result
   save_task_link_in_file $task_id
 }
 
 function save_message_in_file_for_deploy_get_task_info_error() {
   local task_id=$1
+  local raw_message=$2
 
   save_separator_in_file
 
   save_message_header_in_file
   echo "An error occurred while getting information about a task with id $task_id" >> $private_file_path
+  save_raw_message_in_file $raw_message $result
   save_task_link_in_file $task_id
 }
 
 function save_message_in_file_for_add_app_version() {
   local task_id=$1
-  local result=$2
+  local raw_message=$2
+  local result=$3
 
   save_separator_in_file
 
@@ -180,5 +194,6 @@ function save_message_in_file_for_add_app_version() {
       ;;
   esac
 
+  save_raw_message_in_file $raw_message $result
   save_task_link_in_file $task_id
 }
